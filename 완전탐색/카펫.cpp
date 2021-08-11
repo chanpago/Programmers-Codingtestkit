6a72bd18538d1ec02175b350959a34a9fba61b61
chanpago <69561410+chanpago@users.noreply.github.com>
2021-08-11 20:59:15 +0900

    Update 카펫.cpp

--- 완전탐색/카펫.cpp
+++ 완전탐색/카펫.cpp
@@ -9,12 +9,10 @@ vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     int j;
     for (int i = yellow;i >= 0;i--) {
-
         if (yellow % i == 0) {
             if ((i + 2) * 2 + (yellow / i) * 2 == brown) {
                 answer.push_back(i + 2);
                 answer.push_back((yellow / i) + 2);
-
                 break;
             }
         }
